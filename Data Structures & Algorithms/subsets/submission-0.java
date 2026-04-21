fe7eba9aa980e36f6fc8cfbb35b80576e8a5e33a
Rahul Panda <rahulpanda7851@gmail.com>
2026-04-21 17:26:45 +0530

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+
+        HashSet<List<Integer>> ans = new HashSet<>();
+        List<Integer> ds = new ArrayList<>();
+
+        solve(0, nums, ds, ans);
+
+        return new ArrayList<>(ans);
+
+    }
+
+    public void solve(int idx, int arr[], List<Integer> ds, HashSet<List<Integer>> ans){
+         
+         if(idx >= arr.length){
+            ans.add(new ArrayList<>(ds));
+            return;
+         }
+
+         // Include...
+         ds.add(arr[idx]);
+         solve(idx+1, arr, ds, ans);
+         ds.remove(ds.size()-1);
+
+         //Exclude...
+         solve(idx+1, arr, ds, ans);
+
+    }
+}
