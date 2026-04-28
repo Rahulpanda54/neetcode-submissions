b460620e8b42d7e85be754c74686d1741d13acd3
Rahul Panda <rahulpanda7851@gmail.com>
2026-04-28 16:18:05 +0530

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int climbStairs(int n) {
+
+        int dp[]  = new int[n+1];
+
+        for(int i=0; i<dp.length; i++){
+            dp[i] = -1;
+        }
+
+        return solve(n , dp);
+    }
+
+    public int solve(int n, int dp[]){
+
+         if(n <= 1){
+            return 1;
+        }
+
+        if(dp[n] != -1){
+            return dp[n];
+        }
+
+        return dp[n] =  solve(n-1, dp) + solve(n-2, dp);
+    }
+}
