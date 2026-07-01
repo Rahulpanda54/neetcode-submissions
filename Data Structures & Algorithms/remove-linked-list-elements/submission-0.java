77ec2aec4a82d2f9873f264491fa4f56662b7266
Rahul Panda <rahulpanda7851@gmail.com>
2026-07-01 12:46:33 +0530

    Add: remove-linked-list-elements - submission-0

---
+++ Data Structures & Algorithms/remove-linked-list-elements/submission-0.java
@@ -0,0 +1,30 @@
+class Solution {
+    
+    public ListNode removeElements(ListNode head, int val) {
+        
+        if(head == null){
+            return null;
+        }
+
+        if(head.val == val){
+            while(head != null && head.val == val){
+                head = head.next;
+            }
+        }
+
+        ListNode curr = head;   
+
+        while(curr != null && curr.next != null){
+
+            if(curr.next.val == val){
+                curr.next = curr.next.next;
+            }
+
+            else{
+                  curr = curr.next;
+            }
+        }
+
+         return head;
+     }
+ }
